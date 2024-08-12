7610a9586348c61cd8cee5d6bbdb0a85ec9ad0b4
Shubham Singh <shubhamsingh@Shubhams-MacBook-Pro.local>
2024-08-12 21:04:02 +0530

    finalising model implementation

---
+++ Utils/ModelUtils.py
@@ -0,0 +1,169 @@
+from functools import partial
+from inspect import isfunction
+
+import torch
+import math
+
+from einops.layers.torch import Rearrange
+from torch import nn, einsum
+from einops import reduce, rearrange
+import torch.nn.functional as F
+
+
+# conv layer with weight standardisation incorporated
+class WeightStandardizedConv2d(nn.Conv2d):
+    # no need to override init, we will just override forward
+
+    def forward(self, x):
+        # eps is added to denominator to prevent division by zero
+        eps = 1e-5 if x.dtype == torch.float32 else 1e-3
+
+        weight = self.weight
+        mean = reduce(weight, 'o ... -> o 1 1 1', 'mean')
+        var = reduce(weight, 'o ... -> o 1 1 1', partial(torch.var, unbiased=True))
+
+        weight = (weight - mean) / (var + eps).rsqrt()
+
+        x = F.conv2d(x, weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
+        return x
+
+
+class SinusoidalPositionEmbeddings(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, time):
+        device = time.device
+        half_dim = self.dim // 2
+        embeddings = math.log(10000) / (half_dim - 1)
+        embeddings = torch.exp(torch.arange(half_dim, device=device) * -embeddings)
+        embeddings = time[:, None] * embeddings[None, :]
+        embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
+        return embeddings
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads=4, dim_head=32):
+        super().__init__()
+        self.scale = dim_head ** -0.5
+        self.heads = heads
+        hidden_dim = dim_head * heads
+        self.to_qkv = nn.Conv2d(dim, hidden_dim * 3, 1, bias=False)
+        self.to_out = nn.Conv2d(hidden_dim, dim, 1)
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        qkv = self.to_qkv(x).chunk(3, dim=1)
+        q, k, v = map(
+            lambda t: rearrange(t, "b (h c) x y -> b h c (x y)", h=self.heads), qkv
+        )
+        q = q * self.scale
+
+        sim = einsum("b h d i, b h d j -> b h i j", q, k)
+        sim = sim - sim.amax(dim=-1, keepdim=True).detach()
+        attn = sim.softmax(dim=-1)
+
+        out = einsum("b h i j, b h d j -> b h i d", attn, v)
+        out = rearrange(out, "b h (x y) d -> b (h d) x y", x=h, y=w)
+        return self.to_out(out)
+
+
+class LinearAttention(nn.Module):
+    def __init__(self, dim, heads=4, dim_head=32):
+        super().__init__()
+        self.scale = dim_head ** -0.5
+        self.heads = heads
+        hidden_dim = dim_head * heads
+        self.to_qkv = nn.Conv2d(dim, hidden_dim * 3, 1, bias=False)
+
+        self.to_out = nn.Sequential(nn.Conv2d(hidden_dim, dim, 1),
+                                    nn.GroupNorm(1, dim))
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        qkv = self.to_qkv(x).chunk(3, dim=1)
+        q, k, v = map(
+            lambda t: rearrange(t, "b (h c) x y -> b h c (x y)", h=self.heads), qkv
+        )
+
+        q = q.softmax(dim=-2)
+        k = k.softmax(dim=-1)
+
+        q = q * self.scale
+        context = torch.einsum("b h d n, b h e n -> b h d e", k, v)
+
+        out = torch.einsum("b h d e, b h d n -> b h e n", context, q)
+        out = rearrange(out, "b h c (x y) -> b (h c) x y", h=self.heads, x=h, y=w)
+        return self.to_out(out)
+
+
+# The DDPM authors interleave the convolutional/attention layers of the U-Net with group normalization (Wu et al., 2018).
+# Below, we define a PreNorm class, which will be used to apply groupnorm before the attention layer
+class PreNorm(nn.Module):
+    def __init__(self, channels, fn):
+        super().__init__()
+        self.fn = fn
+        self.norm = nn.GroupNorm(1, channels)
+
+    def forward(self, x):
+        x = self.norm(x)
+        x = self.fn(x)
+
+        return x
+
+
+def exists(val):
+    return val is not None
+
+
+def default(val, d):
+    if exists(val):
+        return val
+    return d() if isfunction(d) else d
+
+
+'''
+The code is setting up a sequence of dimension pairs for the neural network architecture.
+
+if init_dim = 64 and dim = 32, the output of this code would be:
+dims = [64, 32, 64, 128, 256]
+in_out = [(64, 32), (32, 64), (64, 128), (128, 256)]
+'''
+def getSequnceOfDimensions(init_dim, dim, dim_mults):
+    dims = [init_dim, *map(lambda m: dim * m, dim_mults)]
+    in_out = list(zip(dims[:-1], dims[1:]))
+    return in_out, dims
+
+
+def num_to_groups(num, divisor):
+    groups = num // divisor
+    remainder = num % divisor
+    arr = [divisor] * groups
+    if remainder > 0:
+        arr.append(remainder)
+    return arr
+
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x, *args, **kwargs):
+        return self.fn(x, *args, **kwargs) + x
+
+
+def Upsample(dim, dim_out=None):
+    return nn.Sequential(
+        nn.Upsample(scale_factor=2, mode="nearest"),
+        nn.Conv2d(dim, default(dim_out, dim), 3, padding=1),
+    )
+
+
+def Downsample(dim, dim_out=None):
+    # No More Strided Convolutions or Pooling
+    return nn.Sequential(
+        Rearrange("b c (h p1) (w p2) -> b (c p1 p2) h w", p1=2, p2=2),
+        nn.Conv2d(dim * 4, default(dim_out, dim), 1),
+    )
--- model/model.py
+++ model/model.py
@@ -1,9 +1,25 @@
 # This is just a u-net
 
+'''
+
+The network is built up as follows:
+
+-> first, a convolutional layer is applied on the batch of noisy images, and position embeddings are computed for the noise levels
+-> next, a sequence of downsampling stages are applied. Each downsampling stage consists of
+    2 ResNet blocks + groupnorm + attention + residual connection + a downsample operation
+-> at the middle of the network, again ResNet blocks are applied, interleaved with attention
+-> next, a sequence of upsampling stages are applied. Each upsampling stage consists of
+    2 ResNet blocks + groupnorm + attention + residual connection + an upsample operation
+-> finally, a ResNet block followed by a convolutional layer is applied.
+'''
+from functools import partial
+
 import torch
 from einops import rearrange
 from torch import nn
-from model.ModelUtils import WeightStandardizedConv2d, exists
+from Utils import WeightStandardizedConv2d, exists, default, getSequnceOfDimensions, \
+    SinusoidalPositionEmbeddings, PreNorm, LinearAttention, Residual, Downsample, Attention, Upsample
+
 
 # Most basic block with conv + groupNorm + Silu activation
 class Block(nn.Module):
@@ -19,10 +35,11 @@ class Block(nn.Module):
 
         if exists(scale_shift):
             scale, shift = scale_shift
-            x = x * (scale+1) + shift
+            x = x * (scale + 1) + shift
 
         x = self.act(x)
         return x
+
 
 # The residual block
 class ResnetBlock(nn.Module):
@@ -52,3 +69,116 @@ class ResnetBlock(nn.Module):
         h = h + self.res_conv(x)
 
         return h
+
+
+class Unet(nn.Module):
+    def __init__(self,
+                 dim,               # dimension of time embedding
+                 init_dim=None,
+                 out_dim=None,      # channels of final output image
+                 dim_mults=(1, 2, 4, 8),
+                 channels=3,
+                 resnet_block_groups=4, ):
+        super().__init__()
+
+        self.channels = channels  # channels in input image
+        input_channels = channels
+
+        init_dim = default(init_dim, dim)
+
+        self.init_conv = nn.Conv2d(input_channels, init_dim, 1, padding=0)
+
+        in_out, dims = getSequnceOfDimensions(init_dim, dim, dim_mults)
+        block_klass = partial(ResnetBlock, groups=resnet_block_groups)
+
+        # time embeddings
+        time_dim = dim * 4
+
+        self.time_mlp = nn.Sequential(
+            SinusoidalPositionEmbeddings(dim),
+            nn.Linear(dim, time_dim),
+            nn.GELU(),
+            nn.Linear(time_dim, time_dim),
+        )
+
+        # layers
+        self.downs = nn.ModuleList([])
+        self.ups = nn.ModuleList([])
+        num_resolutions = len(in_out)  # depth of U-net
+
+        for ind, (dim_in, dim_out) in enumerate(in_out):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.downs.append(
+                nn.ModuleList(
+                    [
+                        block_klass(dim_in, dim_in, time_emb_dim=time_dim),
+                        block_klass(dim_in, dim_in, time_emb_dim=time_dim),
+                        Residual(PreNorm(dim_in, LinearAttention(dim_in))),  # input x; apply group norm, then attention to x, then add x to it.
+                        Downsample(dim_in, dim_out) if not is_last
+                        else nn.Conv2d(dim_in, dim_out, 3, padding=1),
+                    ]
+                )
+            )
+
+        mid_dim = dims[-1]
+        self.mid_block1 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim)
+        self.mid_attn = Residual(PreNorm(mid_dim, Attention(mid_dim)))
+        self.mid_block2 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim)
+
+        for ind, (dim_in, dim_out) in enumerate(reversed(in_out)):
+            is_last = ind == (len(in_out) - 1)
+
+            self.ups.append(
+                nn.ModuleList(
+                    [
+                        block_klass(dim_out + dim_in, dim_out, time_emb_dim=time_dim),
+                        block_klass(dim_out + dim_in, dim_out, time_emb_dim=time_dim),
+                        Residual(PreNorm(dim_out, LinearAttention(dim_out))),
+                        Upsample(dim_out, dim_in)
+                        if not is_last
+                        else nn.Conv2d(dim_out, dim_in, 3, padding=1),
+                    ]
+                )
+            )
+        self.out_dim = default(out_dim, channels)
+
+        self.final_res_block = block_klass(dim * 2, dim, time_emb_dim=time_dim)
+        self.final_conv = nn.Conv2d(dim, self.out_dim, 1)
+
+    def forward(self, x, time):
+        x = self.init_conv(x)
+        r = x.clone()
+
+        t = self.time_mlp(time)
+
+        h = []  # To store bridge connections
+
+        for block1, block2, attn, downsample in self.downs:
+            x = block1(x, t)
+            h.append(x)
+
+            x = block2(x, t)
+            x = attn(x)
+            h.append(x)
+
+            x = downsample(x)
+
+        x = self.mid_block1(x, t)
+        x = self.mid_attn(x)
+        x = self.mid_block2(x, t)
+
+        for block1, block2, attn, upsample in self.ups:
+            x = torch.cat((x, h.pop()), dim=1)  # concat along dim = 1 i.e. channels dimension
+            x = block1(x, t)
+
+            x = torch.cat((x, h.pop()), dim=1)
+            x = block2(x, t)
+            x = attn(x)
+
+            x = upsample(x)
+
+        x = torch.cat((x, r), dim=1)
+
+        x = self.final_res_block(x, t)
+        return self.final_conv(x)
